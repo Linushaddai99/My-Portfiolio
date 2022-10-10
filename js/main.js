56f208256bcef87959adfdc1957cc82b18269348
Linus Shaddai <awajisomegak@gmail.com>
2022-10-10 15:15:31 +0100

    Add Mobile Navigation with Javascript

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const hamburgerMenu = document.querySelector('.hamburger-js');
+const mobileMenu = document.querySelector('.mobile-menu-js');
+const closeMobileMenu = document.querySelector('.close-js');
+const mobileNavigation = document.querySelectorAll('.mobile-menu-option');
+
+
+hamburgerMenu.addEventListener('click', () => {
+    mobileMenu.classList.remove('hide')
+})
+
+closeMobileMenu.addEventListener('click', () => {
+    mobileMenu.classList.add('hide')
+})
+
+mobileNavigation.forEach(item => {
+    item.addEventListener('click', () => {
+        mobileMenu.classList.add('hide')
+    })
+})
