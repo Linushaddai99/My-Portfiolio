e18459ad0294e853263a77168fad70108e8b0b35
Linus Shaddai <awajisomegak@gmail.com>
2022-10-10 15:42:13 +0100

    Linter error correction

--- js/main.js
+++ js/main.js
@@ -3,17 +3,16 @@ const mobileMenu = document.querySelector('.mobile-menu-js');
 const closeMobileMenu = document.querySelector('.close-js');
 const mobileNavigation = document.querySelectorAll('.mobile-menu-option');
 
-
 hamburgerMenu.addEventListener('click', () => {
-    mobileMenu.classList.remove('hide')
-})
+  mobileMenu.classList.remove('hide');
+});
 
 closeMobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.add('hide')
-})
+  mobileMenu.classList.add('hide');
+});
 
 mobileNavigation.forEach(item => {
-    item.addEventListener('click', () => {
-        mobileMenu.classList.add('hide')
-    })
-})
+  item.addEventListener('click', () => {
+      mobileMenu.classList.add('hide');
+  })
+});
