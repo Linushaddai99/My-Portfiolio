f488b9c38e26fe3bfed753b496e63bb1707e0417
Linus Shaddai <awajisomegak@gmail.com>
2022-10-10 18:34:50 +0100

    Fade background for mobile menu

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ const hamburgerMenu = document.querySelector('.hamburger-js');
 const mobileMenu = document.querySelector('.mobile-menu-js');
 const closeMobileMenu = document.querySelector('.close-js');
 const mobileNavigation = document.querySelectorAll('.mobile-menu-option');
+const body = document.querySelector('#element')
 
 hamburgerMenu.addEventListener('click', () => {
   mobileMenu.classList.remove('hide');
+  body.style.filter = "blur(3px)";
+
 });
 
 closeMobileMenu.addEventListener('click', () => {
   mobileMenu.classList.add('hide');
+  body.style.filter = "blur(0)";
 });
 
 mobileNavigation.forEach((item) => {
