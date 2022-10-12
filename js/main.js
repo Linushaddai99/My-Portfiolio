6e0ed70a39e5a1cc1ac05972cf666a0612edf955
Linus Shaddai <awajisomegak@gmail.com>
2022-10-12 07:58:41 +0100

    Fix Linter errors

--- js/main.js
+++ js/main.js
@@ -5,20 +5,6 @@ const closeMobileMenu = document.querySelector('.close-js');
 const mobileNavigation = document.querySelectorAll('.mobile-menu-option');
 const fade = document.querySelector('#element');
 
-// variables for project popup
-const popupWrapper = document.querySelector('.project-popup-wrapper');
-const popupName = document.querySelector('.popup-name');
-const popupPosition = document.querySelector('.popup-position');
-const popupWorkName = document.querySelector('.popup-work-name')
-const popupImage = document.querySelector('.popup-image');
-const popupDescription = document.querySelector('.popup-description');
-const popupTech = document.querySelector('.popup-technologies-container');
-const projectOne = document.querySelector('#project-one');
-const projectTwo = document.querySelector('#project-two');
-const projectThree = document.querySelector('#project-three');
-const projectFour = document.querySelector('#project-four');
-const closePopup = document.querySelector('.close-project-popup');
-
 hamburgerMenu.addEventListener('click', () => {
   mobileMenu.classList.remove('hide');
   fade.style.filter = 'blur(3px)';
@@ -35,8 +21,3 @@ mobileNavigation.forEach((item) => {
     mobileMenu.classList.add('hide');
   });
 });
-
-closePopup.addEventListener('click', () => {
-  popupWrapper.classList.add('hide');
-    fade.style.filter = 'blur(0)';
-})
