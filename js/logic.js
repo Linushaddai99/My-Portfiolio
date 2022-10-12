65e0ee8e40918ec1509bb578b6881cc3979086d9
Linus Shaddai <awajisomegak@gmail.com>
2022-10-12 17:20:36 +0100

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,92 @@
+function createPopup() {
+    const popup = document.createElement('section');
+    popup.classList = 'project-popup-wrapper hide';
+    const projectPopup = document.createElement('div');
+    projectPopup.className = 'project-popup';
+  
+    const popupHeader = document.createElement('div');
+    popupHeader.className = 'popup-header';
+    const h2 = document.createElement('h2');
+    h2.className = 'popup-name';
+    const imageClose = document.createElement('img');
+    imageClose.className = 'close-project-popup';
+    imageClose.setAttribute('src', './img/white-close-icon.svg');
+  
+    popupHeader.appendChild(h2);
+    popupHeader.appendChild(imageClose);
+    projectPopup.appendChild(popupHeader);
+  
+    const popupMain = document.createElement('div');
+  
+    const worksIntroDiv = document.createElement('div');
+    const worksIntro = document.createElement('div');
+    worksIntro.className = 'works-intro';
+    const workName = document.createElement('span');
+    workName.className = 'work-name popup-work-name';
+    const position = document.createElement('ul');
+    position.classList = 'popup-position role';
+  
+    worksIntroDiv.appendChild(worksIntro);
+    worksIntro.appendChild(workName);
+    worksIntro.appendChild(position);
+  
+    popupMain.appendChild(worksIntroDiv);
+  
+    const imagePopup = document.createElement('img');
+    imagePopup.className = 'popup-image';
+    popupMain.appendChild(imagePopup);
+  
+    const popupBody = document.createElement('div');
+    popupBody.className = 'popup-body';
+    const description = document.createElement('p');
+    description.className = 'popup-description';
+  
+    popupBody.appendChild(description);
+  
+    const popupBody2 = document.createElement('div');
+    popupBody2.className = 'popup-body2';
+    const popupTechnologies = document.createElement('div');
+    popupTechnologies.className = 'works-technologies';
+    popupTechnologies.id = 'popup-technologies-container';
+  
+    popupBody2.appendChild(popupTechnologies);
+  
+    const popupLinks = document.createElement('div');
+    popupLinks.className = 'popup-link-container';
+  
+    const demoLink = document.createElement('a');
+    demoLink.className = 'popup-link';
+    demoLink.setAttribute('href', '#');
+    const demoText = document.createTextNode('See live');
+    demoLink.appendChild(demoText);
+    const demoIcon = document.createElement('img');
+    demoIcon.setAttribute('src', './img/source-icon.svg');
+    demoLink.appendChild(demoIcon);
+    popupLinks.appendChild(demoLink);
+  
+    const liveLink = document.createElement('a');
+    liveLink.className = 'popup-link';
+    liveLink.setAttribute('href', '#');
+    const liveText = document.createTextNode('See source');
+    liveLink.appendChild(liveText);
+    const liveIcon = document.createElement('img');
+    liveIcon.setAttribute('src', './img/live-demo-icon.svg');
+    liveLink.appendChild(liveIcon);
+    popupLinks.appendChild(liveLink);
+  
+    popupBody2.appendChild(popupLinks);
+  
+    popupBody.appendChild(popupBody2);
+  
+    popupMain.appendChild(popupBody);
+  
+    projectPopup.appendChild(popupMain);
+  
+    popup.appendChild(projectPopup);
+  
+    document.body.appendChild(popup);
+  }
+
 window.onload = createPopup();
 
 const popupWrapper = document.querySelector('.project-popup-wrapper');
@@ -54,129 +143,40 @@ const popupFour = {
   technologies: ['html', 'css', 'javascript'],
 };
 
-const populateUI = (popupOption) => {
-  popupWrapper.classList.remove('hide');
-  blur.style.filter = 'blur(3px)';
-  popupName.textContent = popupOption.name;
-  popupWorkName.textContent = popupOption.workName;
-  popupPosition.innerHTML = '';
-  popupOption.position.forEach((item) => {
-    popupPosition.innerHTML += `<li>${item}</li>`;
+  const populateUI = (popupOption) => {
+    popupWrapper.classList.remove('hide');
+    blur.style.filter = 'blur(3px)';
+    popupName.textContent = popupOption.name;
+    popupWorkName.textContent = popupOption.workName;
+    popupPosition.innerHTML = '';
+    popupOption.position.forEach((item) => {
+      popupPosition.innerHTML += `<li>${item}</li>`;
+    });
+    popupImage.setAttribute('src', popupOption.imageUrl);
+    popupDescription.textContent = popupOption.description;
+    popupTech.innerHTML = '';
+    popupOption.technologies.forEach((item) => {
+      popupTech.innerHTML += `<li>${item}</li>`;
+    });
+  };
+
+  projectOne.addEventListener('click', () => {
+    populateUI(popupOne);
   });
-  popupImage.setAttribute('src', popupOption.imageUrl);
-  popupDescription.textContent = popupOption.description;
-  popupTech.innerHTML = '';
-  popupOption.technologies.forEach((item) => {
-    popupTech.innerHTML += `<li>${item}</li>`;
+  
+  projectTwo.addEventListener('click', () => {
+    populateUI(popupTwo);
   });
-};
-
-projectOne.addEventListener('click', () => {
-  populateUI(popupOne);
-});
-
-projectTwo.addEventListener('click', () => {
-  populateUI(popupTwo);
-});
-
-projectThree.addEventListener('click', () => {
-  populateUI(popupThree);
-});
-
-projectFour.addEventListener('click', () => {
-  populateUI(popupFour);
-});
-
-closePopup.addEventListener('click', () => {
-  popupWrapper.classList.add('hide');
-  blur.style.filter = 'blur(0)';
-});
-
-function createPopup() {
-  const popup = document.createElement('section');
-  popup.classList = 'project-popup-wrapper hide';
-  const projectPopup = document.createElement('div');
-  projectPopup.className = 'project-popup';
-
-  const popupHeader = document.createElement('div');
-  popupHeader.className = 'popup-header';
-  const h2 = document.createElement('h2');
-  h2.className = 'popup-name';
-  const imageClose = document.createElement('img');
-  imageClose.className = 'close-project-popup';
-  imageClose.setAttribute('src', './img/white-close-icon.svg');
-
-  popupHeader.appendChild(h2);
-  popupHeader.appendChild(imageClose);
-  projectPopup.appendChild(popupHeader);
-
-  const popupMain = document.createElement('div');
-
-  const worksIntroDiv = document.createElement('div');
-  const worksIntro = document.createElement('div');
-  worksIntro.className = 'works-intro';
-  const workName = document.createElement('span');
-  workName.className = 'work-name popup-work-name';
-  const position = document.createElement('ul');
-  position.classList = 'popup-position role';
-
-  worksIntroDiv.appendChild(worksIntro);
-  worksIntro.appendChild(workName);
-  worksIntro.appendChild(position);
-
-  popupMain.appendChild(worksIntroDiv);
-
-  const imagePopup = document.createElement('img');
-  imagePopup.className = 'popup-image';
-  popupMain.appendChild(imagePopup);
-
-  const popupBody = document.createElement('div');
-  popupBody.className = 'popup-body';
-  const description = document.createElement('p');
-  description.className = 'popup-description';
-
-  popupBody.appendChild(description);
-
-  const popupBody2 = document.createElement('div');
-  popupBody2.className = 'popup-body2';
-  const popupTechnologies = document.createElement('div');
-  popupTechnologies.className = 'works-technologies';
-  popupTechnologies.id = 'popup-technologies-container';
-
-  popupBody2.appendChild(popupTechnologies);
-
-  const popupLinks = document.createElement('div');
-  popupLinks.className = 'popup-link-container';
-
-  const demoLink = document.createElement('a');
-  demoLink.className = 'popup-link';
-  demoLink.setAttribute('href', '#');
-  const demoText = document.createTextNode('See live');
-  demoLink.appendChild(demoText);
-  const demoIcon = document.createElement('img');
-  demoIcon.setAttribute('src', './img/source-icon.svg');
-  demoLink.appendChild(demoIcon);
-  popupLinks.appendChild(demoLink);
-
-  const liveLink = document.createElement('a');
-  liveLink.className = 'popup-link';
-  liveLink.setAttribute('href', '#');
-  const liveText = document.createTextNode('See source');
-  liveLink.appendChild(liveText);
-  const liveIcon = document.createElement('img');
-  liveIcon.setAttribute('src', './img/live-demo-icon.svg');
-  liveLink.appendChild(liveIcon);
-  popupLinks.appendChild(liveLink);
-
-  popupBody2.appendChild(popupLinks);
-
-  popupBody.appendChild(popupBody2);
-
-  popupMain.appendChild(popupBody);
-
-  projectPopup.appendChild(popupMain);
-
-  popup.appendChild(projectPopup);
-
-  document.body.appendChild(popup);
-}
+  
+  projectThree.addEventListener('click', () => {
+    populateUI(popupThree);
+  });
+  
+  projectFour.addEventListener('click', () => {
+    populateUI(popupFour);
+  });
+  
+  closePopup.addEventListener('click', () => {
+    popupWrapper.classList.add('hide');
+    blur.style.filter = 'blur(0)';
+  });
