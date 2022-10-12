1d04eb6532db1a6b04b2e7e76f242580199c680c
Linus Shaddai <awajisomegak@gmail.com>
2022-10-12 17:27:54 +0100

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,90 +1,90 @@
 function createPopup() {
-    const popup = document.createElement('section');
-    popup.classList = 'project-popup-wrapper hide';
-    const projectPopup = document.createElement('div');
-    projectPopup.className = 'project-popup';
+  const popup = document.createElement('section');
+  popup.classList = 'project-popup-wrapper hide';
+  const projectPopup = document.createElement('div');
+  projectPopup.className = 'project-popup';
   
-    const popupHeader = document.createElement('div');
-    popupHeader.className = 'popup-header';
-    const h2 = document.createElement('h2');
-    h2.className = 'popup-name';
-    const imageClose = document.createElement('img');
-    imageClose.className = 'close-project-popup';
-    imageClose.setAttribute('src', './img/white-close-icon.svg');
+  const popupHeader = document.createElement('div');
+  popupHeader.className = 'popup-header';
+  const h2 = document.createElement('h2');
+  h2.className = 'popup-name';
+  const imageClose = document.createElement('img');
+  imageClose.className = 'close-project-popup';
+  imageClose.setAttribute('src', './img/white-close-icon.svg');
   
-    popupHeader.appendChild(h2);
-    popupHeader.appendChild(imageClose);
-    projectPopup.appendChild(popupHeader);
+  popupHeader.appendChild(h2);
+  popupHeader.appendChild(imageClose);
+  projectPopup.appendChild(popupHeader);
   
-    const popupMain = document.createElement('div');
+  const popupMain = document.createElement('div');
   
-    const worksIntroDiv = document.createElement('div');
-    const worksIntro = document.createElement('div');
-    worksIntro.className = 'works-intro';
-    const workName = document.createElement('span');
-    workName.className = 'work-name popup-work-name';
-    const position = document.createElement('ul');
-    position.classList = 'popup-position role';
+  const worksIntroDiv = document.createElement('div');
+  const worksIntro = document.createElement('div');
+  worksIntro.className = 'works-intro';
+  const workName = document.createElement('span');
+  workName.className = 'work-name popup-work-name';
+  const position = document.createElement('ul');
+  position.classList = 'popup-position role';
   
-    worksIntroDiv.appendChild(worksIntro);
-    worksIntro.appendChild(workName);
-    worksIntro.appendChild(position);
+  worksIntroDiv.appendChild(worksIntro);
+  worksIntro.appendChild(workName);
+  worksIntro.appendChild(position);
   
-    popupMain.appendChild(worksIntroDiv);
+  popupMain.appendChild(worksIntroDiv);
   
-    const imagePopup = document.createElement('img');
-    imagePopup.className = 'popup-image';
-    popupMain.appendChild(imagePopup);
+  const imagePopup = document.createElement('img');
+  imagePopup.className = 'popup-image';
+  popupMain.appendChild(imagePopup);
   
-    const popupBody = document.createElement('div');
-    popupBody.className = 'popup-body';
-    const description = document.createElement('p');
-    description.className = 'popup-description';
+  const popupBody = document.createElement('div');
+  popupBody.className = 'popup-body';
+  const description = document.createElement('p');
+  description.className = 'popup-description';
   
-    popupBody.appendChild(description);
+  popupBody.appendChild(description);
   
-    const popupBody2 = document.createElement('div');
-    popupBody2.className = 'popup-body2';
-    const popupTechnologies = document.createElement('div');
-    popupTechnologies.className = 'works-technologies';
-    popupTechnologies.id = 'popup-technologies-container';
+  const popupBody2 = document.createElement('div');
+  popupBody2.className = 'popup-body2';
+  const popupTechnologies = document.createElement('div');
+  popupTechnologies.className = 'works-technologies';
+  popupTechnologies.id = 'popup-technologies-container';
   
-    popupBody2.appendChild(popupTechnologies);
+  popupBody2.appendChild(popupTechnologies);
   
-    const popupLinks = document.createElement('div');
-    popupLinks.className = 'popup-link-container';
+  const popupLinks = document.createElement('div');
+  popupLinks.className = 'popup-link-container';
   
-    const demoLink = document.createElement('a');
-    demoLink.className = 'popup-link';
-    demoLink.setAttribute('href', '#');
-    const demoText = document.createTextNode('See live');
-    demoLink.appendChild(demoText);
-    const demoIcon = document.createElement('img');
-    demoIcon.setAttribute('src', './img/source-icon.svg');
-    demoLink.appendChild(demoIcon);
-    popupLinks.appendChild(demoLink);
+  const demoLink = document.createElement('a');
+  demoLink.className = 'popup-link';
+  demoLink.setAttribute('href', '#');
+  const demoText = document.createTextNode('See live');
+  demoLink.appendChild(demoText);
+  const demoIcon = document.createElement('img');
+  demoIcon.setAttribute('src', './img/source-icon.svg');
+  demoLink.appendChild(demoIcon);
+  popupLinks.appendChild(demoLink);
   
-    const liveLink = document.createElement('a');
-    liveLink.className = 'popup-link';
-    liveLink.setAttribute('href', '#');
-    const liveText = document.createTextNode('See source');
-    liveLink.appendChild(liveText);
-    const liveIcon = document.createElement('img');
-    liveIcon.setAttribute('src', './img/live-demo-icon.svg');
-    liveLink.appendChild(liveIcon);
-    popupLinks.appendChild(liveLink);
+  const liveLink = document.createElement('a');
+  liveLink.className = 'popup-link';
+  liveLink.setAttribute('href', '#');
+  const liveText = document.createTextNode('See source');
+  liveLink.appendChild(liveText);
+  const liveIcon = document.createElement('img');
+  liveIcon.setAttribute('src', './img/live-demo-icon.svg');
+  liveLink.appendChild(liveIcon);
+  popupLinks.appendChild(liveLink);
   
-    popupBody2.appendChild(popupLinks);
+  popupBody2.appendChild(popupLinks);
   
-    popupBody.appendChild(popupBody2);
+  popupBody.appendChild(popupBody2);
   
-    popupMain.appendChild(popupBody);
+  popupMain.appendChild(popupBody);
   
-    projectPopup.appendChild(popupMain);
+  projectPopup.appendChild(popupMain);
   
-    popup.appendChild(projectPopup);
+  popup.appendChild(projectPopup);
   
-    document.body.appendChild(popup);
+  document.body.appendChild(popup);
   }
 
 window.onload = createPopup();
