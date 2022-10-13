2459ba9e7e61bbdac86cb37266eafec8de2d4fc9
Linus Shaddai <awajisomegak@gmail.com>
2022-10-13 07:33:04 +0100

    Update logic.js

--- js/logic.js
+++ js/logic.js
@@ -136,45 +136,6 @@ const projectDatas = [
     technologies: ['html', 'css', 'javascript'],
   }
 ]
-// const popupOne = {
-//   name: 'Tonic',
-//   workName: 'CANOPY',
-//   position: ['Back End Dev', '2015'],
-//   imageUrl: './img/Snapshoot Portfolio.svg',
-//   description:
-//     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and',
-//   technologies: ['html', 'css', 'javascript'],
-// };
-
-// const popupTwo = {
-//   name: 'Multi-Post Stories',
-//   workName: 'FACEBOOK',
-//   position: ['Full Stack Dev', '2015'],
-//   imageUrl: './img/Snapshoot Portfolio2.svg',
-//   description:
-//     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and',
-//   technologies: ['html', 'css', 'javascript'],
-// };
-
-// const popupThree = {
-//   name: 'Facebook 360',
-//   workName: 'FACEBOOK',
-//   position: ['Full Stack Dev', '2015'],
-//   imageUrl: './img/Snapshoot Portfolio3.svg',
-//   description:
-//     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and',
-//   technologies: ['html', 'css', 'javascript'],
-// };
-
-// const popupFour = {
-//   name: 'Uber Navigation',
-//   workName: 'Uber',
-//   position: ['Lead Dev', '2015'],
-//   imageUrl: './img/Snapshoot Portfolio4.svg',
-//   description:
-//     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and',
-//   technologies: ['html', 'css', 'javascript'],
-// };
 
 const populateUI = (popupOption) => {
   popupWrapper.classList.remove('hide');
@@ -194,19 +155,19 @@ const populateUI = (popupOption) => {
 };
 
 projectOne.addEventListener('click', () => {
-  populateUI(popupOne);
+  populateUI(projectDatas[0]);
 });
 
 projectTwo.addEventListener('click', () => {
-  populateUI(popupTwo);
+  populateUI(projectDatas[1]);
 });
 
 projectThree.addEventListener('click', () => {
-  populateUI(popupThree);
+  populateUI(projectDatas[2]);
 });
 
 projectFour.addEventListener('click', () => {
-  populateUI(popupFour);
+  populateUI(projectDatas[3]);
 });
 
 closePopup.addEventListener('click', () => {
